a10b4d7abab154958d4dd7c69c92fb4ed06f1276
MuhRandy <muhammadrandy0027@gmail.com>
2024-10-16 08:01:56 +0800

    feat: add depth, isBalanced, and rebalance in Tree method

--- main.js
+++ main.js
@@ -6,25 +6,27 @@ const tree = new Tree([1, 2, 3, 5, 4, 3, 6, 7, 9]);
 
 // console.log(util.inspect(tree, { depth: null }));
 
-// tree.insert(2);
-tree.insert(-7);
-// tree.insert(7);
-// console.log(tree.find(10));
-// console.log(tree.levelOrder());
-// console.log(typeof logValue);
-// tree.levelOrder((value) => console.log(value));
-// console.log(tree.inOrder());
-// tree.inOrder((value) => console.log(value));
-// tree.preOrder((v) => console.log(v));
-// tree.postOrder((v) => console.log(v));
+// // tree.insert(2);
+// tree.insert(-7);
+// // tree.insert(7);
+// // console.log(tree.find(10));
+// // console.log(tree.levelOrder());
+// // console.log(typeof logValue);
+// // tree.levelOrder((value) => console.log(value));
+// // console.log(tree.inOrder());
+// // tree.inOrder((value) => console.log(value));
+// // tree.preOrder((v) => console.log(v));
+// // tree.postOrder((v) => console.log(v));
 
-// console.log(util.inspect(tree, { depth: null }));
-tree.prettyPrint();
-// console.log(tree.height(3));
-// console.log(tree.height(2));
-// console.log(tree.height(1));
+// // console.log(util.inspect(tree, { depth: null }));
+// tree.prettyPrint();
+// // console.log(tree.height(3));
+// // console.log(tree.height(2));
+// // console.log(tree.height(1));
 
-console.log(tree.depth(7));
-// console.log(tree.isBalanced());
-tree.rebalance();
-tree.prettyPrint();
+// console.log(tree.depth(7));
+// // console.log(tree.isBalanced());
+// tree.rebalance();
+// tree.prettyPrint();
+
+console.log(Math.floor(Math.random() * 100));
